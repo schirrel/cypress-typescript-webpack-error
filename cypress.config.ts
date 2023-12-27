2eac6eb824b1ebe6f344daa6cdb4d6e1a94c9085
Alan Schio <6757777+schirrel@users.noreply.github.com>
2023-12-27 18:00:46 -0400

    Adding sample config files

---
+++ cypress.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from 'cypress';
+import * as path from 'path';
+import { env } from './config/env';
+import { defaultPaths, e2ePaths } from './config/paths';
+
+console.log(path.resolve(__dirname, './src/framework'));
+const config = {
+  ...defaultPaths,
+  reporter: 'cypress-mochawesome-reporter',
+  env,
+  e2e: {
+    ...e2ePaths,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+      // @src alias
+      const options = {
+        webpackOptions: {
+          module: {
+            rules: [
+              {
+                test: /\.ts?$/,
+                exclude: [/node_modules/],
+                use: [
+                  {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env'],
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+          resolve: {
+            extensions: ['.ts', '.js'],
+            alias: {
+              '@': path.resolve(__dirname, './src'),
+            },
+          },
+        },
+        watchOptions: {},
+      };
+      on('file:preprocessor', webpackPreprocessor(options));
+      return config;
+    },
+  },
+  viewportWidth: 1440,
+  viewportHeight: 900,
+  defaultCommandTimeout: 60000,
+  requestTimeout: 60000,
+  // so we can handle iframes
+  chromeWebSecurity: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: 'report',
+  },
+};
+export default defineConfig(config);
